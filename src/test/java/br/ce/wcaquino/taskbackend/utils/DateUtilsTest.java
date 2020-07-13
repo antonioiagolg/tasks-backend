62f399719aa11eda6555dd7b4c37a1da15647d8f
Antonio Iago Lemos Gaspar <antonio.iagolg@gmail.com>
2020-07-13 15:32:57 +0200

    Corrigindo assertion para datas passadas

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -13,7 +13,7 @@ public class DateUtilsTest {
     @Test
     public void deveRetornarFalseParaDatasPassadas() {
         LocalDate date = LocalDate.of(2010, 1, 1);
-        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
     }
 
     @Test
